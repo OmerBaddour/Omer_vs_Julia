27777e77037ce6b9cf3278ccacac06664c6bca65
Omer <ob2267@columbia.edu>
2019-12-27 23:13:39 +0200

    First final product complete

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
 
-console.log('Here\'s a hidden message');
+console.log('You already know');
